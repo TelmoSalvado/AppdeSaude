f5cc9ccddd62afa6128f3598c0c44e1ace28dd5c
TelmoSalvado <48351064+TelmoSalvado@users.noreply.github.com>
2022-05-12 17:27:55 +0100

    Adicionar arquivos de projeto.

---
+++ AppdeSaude/Controllers/UtilizadorsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AppdeSaude.Models;
+
+namespace AppdeSaude.Controllers
+{
+    public class UtilizadorsController : Controller
+    {
+        private AppSaudeEntities2 db = new AppSaudeEntities2();
+
+        // GET: Utilizadors
+        public ActionResult Index()
+        {
+            var utilizador = db.utilizador.Include(u => u.localidade);
+            return View(utilizador.ToList());
+        }
+
+        // GET: Utilizadors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            utilizador utilizador = db.utilizador.Find(id);
+            if (utilizador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilizador);
+        }
+
+        // GET: Utilizadors/Create
+        public ActionResult Create()
+        {
+            ViewBag.localidade_localidade_id = new SelectList(db.localidade, "localidade_id", "Cidade");
+            return View();
+        }
+
+        // POST: Utilizadors/Create
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Nome,Contacto,Nrutente,Datadenascimento,Genero,Altura,Peso,Email,Password,CreatedAt,localidade_localidade_id")] utilizador utilizador)
+        {
+            if (ModelState.IsValid)
+            {
+                utilizador.CreatedAt = DateTime.Now;
+                db.utilizador.Add(utilizador);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.localidade_localidade_id = new SelectList(db.localidade, "localidade_id", "Cidade", utilizador.localidade_localidade_id);
+            return View(utilizador);
+        }
+
+        // GET: Utilizadors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            utilizador utilizador = db.utilizador.Find(id);
+            if (utilizador == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.localidade_localidade_id = new SelectList(db.localidade, "localidade_id", "Cidade", utilizador.localidade_localidade_id);
+            return View(utilizador);
+        }
+
+        // POST: Utilizadors/Edit/5
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Nome,Contacto,Nrutente,Datadenascimento,Genero,Altura,Peso,Email,Password,CreatedAt,localidade_localidade_id")] utilizador utilizador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(utilizador).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.localidade_localidade_id = new SelectList(db.localidade, "localidade_id", "Cidade", utilizador.localidade_localidade_id);
+            return View(utilizador);
+        }
+
+        // GET: Utilizadors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            utilizador utilizador = db.utilizador.Find(id);
+            if (utilizador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilizador);
+        }
+
+        // POST: Utilizadors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            utilizador utilizador = db.utilizador.Find(id);
+            db.utilizador.Remove(utilizador);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AppdeSaude/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppdeSaude.MvcApplication" Language="C#" %>
